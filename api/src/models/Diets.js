cb03baf08b2c92e84bdcf3cf50a154d937fd5fc3
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-05-09 23:22:03 -0300

    comenzando api

---
+++ api/src/models/Diets.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "diets",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.STRING,
+      },
+      health_score: {
+        type: DataTypes.FLOAT,
+        validate: {
+          min: 0,
+          max: 100,
+        },
+      },
+      instructions: {
+        type: DataTypes.ARRAY(DataTypes.TEXT),
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
