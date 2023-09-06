059b2ca1d25ecab3d9e4de466cf0cef63eeba1cc
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-09-06 09:13:02 -0300

    modificacion redux

--- api/src/app.js
+++ api/src/app.js
@@ -7,6 +7,7 @@ const indexRouter = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
+const { PORT_CORS } = process.env;
 
 server.name = 'API';
 
@@ -15,7 +16,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', PORT_CORS); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
